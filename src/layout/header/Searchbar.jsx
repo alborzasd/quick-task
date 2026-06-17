f40033067963b4c4d54e5737b8eca2e20167033f
alborzasd <alborzasd@gmail.com>
2026-06-17 11:01:04 +0330

    Implement search

--- src/layout/header/Searchbar.jsx
+++ src/layout/header/Searchbar.jsx
@@ -1,14 +1,23 @@
 import { useEffect, useRef } from "react";
+import { useSearchParams } from "react-router";
 
 function Searchbar() {
   let inputRef = useRef(null);
+  let [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     inputRef.current.focus();
   }, []);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const query = inputRef?.current?.value;
+
+    setSearchParams(query ? { q: query } : {}); 
+  }
+
   return (
-    <form className="flex-1">
+    <form className="flex-1" onSubmit={handleSubmit}>
       <input
         ref={inputRef}
         className="w-full px-1 py-2 border-2 rounded-md border-blue-300"
--- src/layout/main/task-list/TaskList.jsx
+++ src/layout/main/task-list/TaskList.jsx
@@ -1,14 +1,26 @@
 import { useFolders } from "../../../data/folders/useFolders";
 import { useTasks } from "../../../data/tasks/useTasks";
 import TaskItem from "./TaskItem";
+import { useSearchParams } from "react-router";
 
 function TaskList() {
+  let [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("q") ?? "";
+
   const { tasks } = useTasks();
+
+  const filteredTasks = tasks.filter(
+    (task) =>
+      task.note.toLowerCase().includes(query.toLowerCase()) ||
+      task.title.toLowerCase().includes(query.toLowerCase()),
+  );
+  // console.log("filteredTasks", filteredTasks, query);
+
   const { folders } = useFolders();
 
   return (
     <ul className="mt-5 flex flex-col gap-5">
-      {tasks.map((task) => (
+      {filteredTasks.map((task) => (
         <TaskItem task={task} folders={folders} key={task.id} />
       ))}
     </ul>
